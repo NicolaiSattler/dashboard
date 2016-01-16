1c8c4ba3b48043f741b04be7eee248797279eb73
Nicolai Sattler <nicolaisattler1990@gmail.com>
2016-01-16 17:02:05 +0100

    bla

--- code/lib/HTTPRequestProto.js
+++ code/lib/HTTPRequestProto.js
@@ -1,41 +1,73 @@
-//namespace
-var JSLibrary = JSLibrary || {    
-    //class   
-    HTTPRequest: function () {
-        //methods: create a Cross-Origin Resource Sharing Request.        
-        function CreateCORSRequest(method, url) {
-            var xhr = new XMLHttpRequest();
-
-            if ("withCredentials" in this.xhr) {
-                xhr.open(method, url, true);
+var HTTPRequestPrototype = {
+    CORSRequest: function (method, url) {
+        var xhr = new XMLHttpRequest();
+        var url = url;
+        xhr.onreadystatechange = function (){
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                return xhr.responseBody;
             }
-            else if (typeof XDomainRequest != "undefined") {
-                //Excists only in IE: 
-                xhr = new XDomainRequest();
+        }
+        xhr.onabort = function (e) {
+            console.log("Request aborted:" + url);
+            console.log(e.returnValue);
+        }
+        xhr.ontimeout = function (e) {
+            console.log("Request timeout:" + url);
+            console.log(e.returnValue);
+        }
+        xhr.onerror = function (e) {
+            console.log("An error has occured:" + url);
+            console.log(e.returnValue);
+        }
+
+        if ("withCredentials" in xhr) {
+            xhr.open(method, url, true);
+            xhr.send(null);
+        }
+        else if (typeof XDomainRequest != "undefined") {
+            //Excists only in IE: 
+            xhr = new XDomainRequest();
+        }
+        else {
+            //CORS is not supported
+            xhr = null;
+        }
+    },
+
+    GetRSSFeed: function (url) {
+        if (window.ActiveXObject) {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        else if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        }
+        else {
+            alert("Your browser does not support AJAX.");
+        }
+
+        xhr.open("GET", url, true);
+
+        //request for fresh (non-cached_ data.
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.setRequestHeader("Pragma", "no-cache");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                if (xhr.responseText != null) {
+                    return xhr.responseXML;
+                }
+                else {
+                    console.log("Could not load RSS:" + url);
+                    return false;
+                }
             }
             else {
-                //CORS is not supported
-                xhr = null;
+                console.log("Error:" + xhr.status + " Received:" + xhr.statusText);
             }
-
-            //Events
-            xhr.onabort = function (e) {
-                console.log("Request aborted:" + url);
-                console.log(e.returnValue);
-            }
-
-            xhr.ontimeout = function (e) {
-                console.log("Request timeout:" + url);
-                console.log(e.returnValue);
-            }
-
-            xhr.onerror = function (e) {
-                console.log("An error has occured:" + url);
-                console.log(e.returnValue);
-            }
-        };
+        }
+        xhr.send(null);
     }
-};
+}
 
 
 
--- code/weatherfeed.js
+++ code/weatherfeed.js
@@ -1,12 +1,6 @@
-window.onload = function()
-{
-    populateSelect();
-    retrieveDataFromRSS()
-}
-
-var lib = JSLibrary;
-var RSSWeatherURL = "http://www.rssweather.com/wx/nl/leeuwarden/rss.php";
-var Cities = new Array
+this._weatherURL = "http://www.xml.com/2006/09/13/examples/google-rss.xml";
+this._httpRequest = Object.create(HTTPRequestPrototype);
+this._cities = new Array
     ("Amsterdam", 
     "De Bilt", 
     "De Kooy", 
@@ -24,27 +18,33 @@ var Cities = new Array
     "Volkel",
     "Woensdrecht");
 
+///https://developer.yahoo.com/weather/#get-started
 
+
+window.onload = function()
+{
+    populateSelect();
+    retrieveDataFromRSS();
+}
+  
 function populateSelect(){
-    var selectCity = document.getElementById("city");
-    for(var i = 0; i < Cities.length; i++){
-        var cityOption = document.createElement('option');
-        cityOption.value, cityOption.label, cityOption.text = Cities[i];
+    var selectCity = document.getElementById('city');
+    for(var i = 0; i < _cities.length; i++){
+        var cityOption = new Option(_cities[i], _cities[i], false, false);
+        cityOption.value, cityOption.label, cityOption.text = _cities[i];
         selectCity.appendChild(cityOption);
     }      
 }
+     
+function retrieveDataFromRSS() {
+    var result = _httpRequest.GetRSSFeed(_weatherURL);
 
+    //corsRequest.onreadystatechange = function () {
+    //    if (corsRequest.readyState == 4 && corsRequest.status == 200) {
+    //        var responseHeader = corsRequest.getAllResponseHeaders();
+    //    }
+    //};
 
-function retrieveDataFromRSS(url) {
+    //corsRequest.open("GET", url, true);
     
-    var http = new lib.HTTPRequest();
-    var corsRequest = http.createCORSRequest("GET", url);  
-    corsRequest.onreadystatechange = function () {
-        if (corsRequest.readyState == 4 && corsRequest.status == 200) {
-            var responseHeader = corsRequest.getAllResponseHeaders();
-        }
-    };
-
-    corsRequest.open("GET", url, true);
-    corsRequest.send();
 }
