ff44baa87a507ff4caad32888caf6ca51b6944b9
nieksa <nicolaisattler1990git config --global user.email nicolaisattler1990@gmail.com>
2016-01-17 19:55:44 +0100

    draggable

---
+++ code/grid.js
@@ -0,0 +1,21 @@
+﻿var currentCellIndex;
+var newCellIndex;
+var grid = document.getElementsByClassName("grid");
+var cells = document.getElementsByClassName("cell");
+
+
+function SetGridToDraggable() {
+    for (var i = 0; i < cells.length; i++) {
+        var obj = cells[i];
+        obj.onclick = cellOnClick;
+    }
+}
+
+function cellOnClick(e) {
+    currentCellIndex = $(this).index();
+    console.log("Cell " + currentCellIndex + " has been clicked.");
+}
+
+
+
+
