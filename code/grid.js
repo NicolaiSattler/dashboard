df93b7eb05e34271d172475847009bb33ff27d24
nieksa <nicolaisattler1990git config --global user.email nicolaisattler1990@gmail.com>
2016-01-23 12:49:42 +0100

    grid draggable start..  +HTML 5 structure added

--- code/grid.js
+++ code/grid.js
@@ -1,5 +1,7 @@
-﻿var currentCellIndex;
-var newCellIndex;
+﻿var SelectedCellIndex;
+var SelectedCellId;
+var _event = false;
+
 var grid = document.getElementsByClassName("grid");
 var cells = document.getElementsByClassName("cell");
 
@@ -7,15 +9,33 @@ var cells = document.getElementsByClassName("cell");
 function SetGridToDraggable() {
     for (var i = 0; i < cells.length; i++) {
         var obj = cells[i];
-        obj.onclick = cellOnClick;
+        obj.addEventListener("mousedown", GetCellAttributes);
     }
 }
 
-function cellOnClick(e) {
-    currentCellIndex = $(this).index();
-    console.log("Cell " + currentCellIndex + " has been clicked.");
+function GetCellAttributes(e) {
+    SelectedCellIndex = $(this).index();
+    SelectedCellId = $(this).attr('id');
+
+    console.log("Cell:" + SelectedCellId);
+    _event = true;
 }
 
+document.onmousemove = function (e) {
+    if (!_event) return;
 
+    var x = e.pageX;
+    var y = e.pageY;
+    var cell = document.getElementById(SelectedCellId);
+
+    cell.style.position = "absolute";
+    cell.style.left = x + "px";
+    cell.style.top = y + "px";
+    cell.style.zIndex = cell.style.zIndex + 1;
+}
+
+document.onmouseup = function (e) {
+    _event = false;
+}
 
 
